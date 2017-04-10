d934d644c89a2fcfdd4b7b1904e993f338d9ebdd
Yeqiao Wang <ee07b291@gmail.com>
2017-04-09 19:19:09 -0700

    .bashrc: Add custom '.bashrc'.
    The original way of patching '~/.bashrc' actually messes up with the OSX
    bash.  So, let's just use a customized, working one everywhere.
    
    Signed-off-by: Yeqiao Wang <ee07b291@gmail.com>

--- install.sh
+++ install.sh
@@ -27,39 +27,3 @@ ln -s $FORCE $PWD/.alex_bin ~/.alex_bin
 for f in `find . -maxdepth 1 -type f -name '.*'`; do
     ln -s $FORCE $PWD/$f ~ || help
 done
-
-# modify the ~/.bashrc
-if grep -- '## alexw_config ##' ~/.bashrc 2>&1 1>/dev/null; then
-    printf ".bashrc already configured, REMOVE IT.\n"
-    sed -i '/alexw_config/,/alexw_config/d' ~/.bashrc
-fi
-
-printf "
-########## alexw_config ##########
-
-# tmux attach (inspired by Joe Stringer).
-function tmux_attach () {
-   local session_id=0
-   local client_id=\${session_id}+\$(date +%%Y%%m%%d%%H%%M%%S)
-   local empty_sessions=\$(tmux ls | grep -v attached | grep -v \${session_id}: | cut -d : -f1)
-   local idle=
-
-   for idle in \${empty_sessions}; do
-      tmux kill-session -t \${idle}
-   done
-
-   tmux new-session -d -t \${session_id} -s \${client_id}
-   tmux attach-session -t \${client_id}
-   tmux kill-session -t \${client_id}
-}
-
-alias rmt='rm *~'
-alias rmh='rm \#*'
-alias wn1='watch -n1 -d'
-alias ll='ls -alrth'
-
-export PATH=\$PATH:\$HOME/.alex_bin
-export EDITOR=/usr/bin/emacs
-export LC_ALL=C
-########## alexw_config ##########
-" >> ~/.bashrc
